647025fbef8b76d1679f3c6a116799f1b2757315
Gaurav Soni <gsoni8158@gmail.com>
2023-05-29 01:52:42 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import React from "react";
  import './../styles/App.css';
@@ -9,18 +8,7 @@ const App = () => {
     <div>
         {/* Do not remove the main div */}
         <Search/>
-=======
-import React from "react";
-import '../styles/App.css';
-
-const App = () => {
-  return (
-
-    <div id="main">
-      <p>I am learning React. My life is getting better.</p>
->>>>>>> 7068fe34c78fe298d0fa0a7cf5299da54dc5a791
     </div>
-
   )
 }
 
